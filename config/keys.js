724da44f81cc9a3b187d4f35385e6d9bc39db79f
Streple <streple@streple.com>
2024-04-21 06:02:33 -0400

    all

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword33 = 'mongodb://localhost:27017/mern2';
+dbPassword = 'mongodb+srv://cardsequel:Cardano121@cluster0.8jzknzq.mongodb.net/CardsequelUsers'
+module.exports = {
+    mongoURI: dbPassword
+};
